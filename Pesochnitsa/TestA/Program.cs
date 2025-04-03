e823e32b9e054a1c78584c57dd71d2f888f1139f
Adelina Sharafutdinova <adelisha.dinov@gmail.com>
2025-04-03 11:21:31 +0300

    first commit

---
+++ Pesochnitsa/TestA/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        int N = int.Parse(Console.ReadLine());
+        int maxCount = 0;
+        string bestString = "";
+
+        for (int i = 0; i < N; i++)
+        {
+            string current = Console.ReadLine();
+            int uniqueCount = new HashSet<char>(current).Count;
+            
+            if (uniqueCount > maxCount)
+            {
+                maxCount = uniqueCount;
+                bestString = current;
+            }
+        }
+        
+        Console.WriteLine($"{maxCount} {bestString}");
+    }
+}
