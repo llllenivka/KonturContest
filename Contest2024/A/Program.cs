476311a04130526d9a21c3b663bf13ec097a11c4
Adelina Sharafutdinova <adelisha.dinov@gmail.com>
2025-04-03 11:52:20 +0300

    add: A

--- Contest2024/A/Program.cs
+++ Contest2024/A/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split(" ");
+            
+        long T = long.Parse(input[1]);
+        
+        long minCount = Console.ReadLine().Split(" ").Select(long.Parse).ToArray().Sum();
+        long maxCount = Console.ReadLine().Split(" ").Select(long.Parse).ToArray().Sum();
+        
+        if(T < minCount || T > maxCount) Console.Write("NO");
+        else Console.Write("YES");
+        
+         
+
+    }
+}
+
+
